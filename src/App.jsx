ea19acd68b9444261043ddeb6c52ace179c26018
Spencer <sreynolds.jvhs@gmail.com>
2026-05-22 15:55:33 -0500

    added filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function App() {
+  const [filter, setFilter] = useState("all");
   const [input,setInput] = useState("");
   const [tasks, setTasks] = useState([]);
   
@@ -8,12 +9,39 @@ function App() {
     if (input.trim() === "") return;
 
     const newTask = {
-      text: input
+      id: Date.now(),
+      text: input,
+      completed: false
     };
 
     setTasks([...tasks,newTask]);
     setInput("");
   }
+
+  function deleteTask(id){
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
+  }
+
+  function toggleTask(id) {
+    const updatedTasks = tasks.map(
+      task => task.id === id
+      ? {...task, completed: !task.completed}
+      : task
+    );
+
+    setTasks(updatedTasks);
+  }
+
+  const filteredTasks = tasks.filter(task => {
+    if (filter === "active") {
+      return !task.completed;
+    }
+    if (filter === "completed") {
+      return task.completed;
+    }
+    return true;
+  });
 
   return (
     <div>
@@ -23,8 +51,21 @@ function App() {
 
       <button onClick={addTask}>Add Task</button>
 
+      <div>
+        <button onClick={() => setFilter("all")}>All</button>
+        <button onClick={() => setFilter("active")}>Active</button>
+        <button onClick={() => setFilter("completed")}>Completed</button>
+      </div>
+
       <ul>
-        {tasks.map((task,index) => (<li key={index}>{task.text}</li>))}
+        {filteredTasks.map((task) => (
+          <li key={task.id}>
+            <span onClick={() => toggleTask(task.id)} style={{textDecoration: task.completed ? "line-through" : "none", cursor: "pointer"}}>
+              {task.text}
+            </span>
+
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>))}
       </ul>
     </div>
   );
